59664bcb3293ea6d354f3aeee368c6d67e990acd
abxalon <44273819+abxalon@users.noreply.github.com>
2023-07-03 19:17:43 -0600

    [UPDATE] Second implementation, 100% working

--- rps.js
+++ rps.js
@@ -1,47 +1,35 @@
-    
-    
-    // Create a function that asks the user for a choice, and store it in a variable named "userChoice"
-    // userChoice is then stored in lowercase for further use
-    function getUserChoice(){
-        let userChoice = prompt("We are playing rock, paper, scissors. Your choice is:");
-        return userChoice.toLowerCase();
-    }
 
-    
-    // Create a function that gets a value between zero and two (considering array index constraints)
-    function getComputerChoice(){
-        // Create an array with 3 values [Rock, Paper scissors]
-        const options = ["rock", "paper", "scissors"];
-        // compChoice gets a random Math value and looks it up inside the array
-        let compChoice = options.at(Math.floor(Math.random()*3));
-        // value is returned as result of the function
-        return compChoice;
-    }
+    // Create an array of options for compPlays selection
+    // Create userPlays variable from a case insensitive user input
+    // Create compPlays variable from selecting an option from the array through index pointing by a random rounded number
+    const options = ["rock", "paper", "scissors"];
+    let userPlays = prompt("We are playing rock, paper, scissors. Your choice is:").toLocaleLowerCase();
+    let compPlays = options.at(Math.floor(Math.random()*3));
 
-    console.log(getUserChoice());
-    console.log(getComputerChoice());
-
-    let userPlays = getUserChoice();
-    let comPlays = getComputerChoice();
-
-    // Create a function that plays a single round
+    // Create a function that plays a single round, comparing results
     function playRound(playerSelection, computerSelection){
-        let userPlays = getUserChoice();
-        let compPlays = getComputerChoice();
-
-        if(userPlays === "rock" && compPlays === "paper"){
+        if(playerSelection === computerSelection){
+            console.log("It's a tie!");
+        } else if(playerSelection === "rock" && computerSelection === "paper"){
             console.log("You loose!, paper beats rock");
-        } else if(userPlays === "paper" && compPlays === "scissors") {
+        } else if(playerSelection === "paper" && computerSelection === "scissors") {
             console.log("You loose!, scissors beat paper");
-        } else if(userPlays === "scissors" && compPlays === "rock") {
+        } else if(playerSelection === "scissors" && computerSelection === "rock") {
             console.log("You loose!, rock beats scissors");
-        } else if(userPlays === "paper" && compPlays === "rock") {
+        } else if(playerSelection === "paper" && computerSelection === "rock") {
             console.log("You win!, paper beats rock");
-        } else if(userPlays === "rock" && compPlays === "scissors"){
+        } else if(playerSelection === "rock" && computerSelection === "scissors"){
             console.log("You win!, rock beats scissors");
-        } else if(userPlays === "scissors" && compPlays  === "paper"){
+        } else if(playerSelection === "scissors" && computerSelection  === "paper"){
             console.log("You win!, scissors beat paper");
         }
-        return;
     }
+
+    // Invoke playRound function with user input and comp random selection
+    playRound(userPlays,compPlays);
+
+    // Log player and computer moves
+    console.log("User played: " + userPlays);
+    console.log("Computar played: " + compPlays);
+    
     
